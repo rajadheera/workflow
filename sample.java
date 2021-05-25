ccb784926ce775b762edf7b37906e3b0e5d1d242
rajadheera <84629786+rajadheera@users.noreply.github.com>
2021-05-25 18:05:58 +0530

    Revert "initial build structure for java spring using maven"

---
+++ sample.java
@@ -0,0 +1,13 @@
+git pull( fetch + merge)
+
+git clone
+
+git add . (add all the files into staging area )
+
+git add file1 file2 ---staging
+
+git commit -m"JIRA-101 changes for USER_story-102"
+
+git push origin main
+
+git pull --all
