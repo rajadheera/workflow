b790ff4df5af1757b0025c082257c54ad6a852a5
raja <rajadheeravath@gmail.com>
2021-05-27 10:59:30 +0530

    change for merge prac

--- sample.java
+++ sample.java
@@ -2,7 +2,8 @@ class sample
 {
 public static void main (String[])
 {
-system.out.println(day5 code  was merge by dev2 );
+system.out.println(day2 change1 );
+system.out.println(day5 change1 );
 
 }
 }
